53a15a4245ad6729eca385e5a90055f1dab9922b
wannaGetCode <ducquang2503@gmail.com>
2022-06-21 17:54:37 +0700

    Fix Search component

--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
+import { collection, getDocs } from 'firebase/firestore'
 import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import HeadlessTippy from '@tippyjs/react/headless'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -6,10 +7,11 @@ import classNames from 'classnames/bind'
 import styles from './Search.module.scss'
 
 import { SearchIcon } from '~/components/Icons'
-import { useDebounce } from '~/hooks'
-import * as searchService from '~/services/searchService'
+// import { useDebounce } from '~/hooks'
+// import * as searchService from '~/services/searchService'
 import AccountItem from '~/components/AccountItem'
 import { Wrapper as PopperWrapper } from '~/components/Popper'
+import db from '~/firebase'
 
 const cx = classNames.bind(styles)
 
@@ -19,7 +21,7 @@ const Search = () => {
   const [showResult, setShowResult] = useState(false)
   const [loading, setLoading] = useState(false)
 
-  const debouncedSearchValue = useDebounce(searchValue, 500)
+  // const debouncedSearchValue = useDebounce(searchValue, 500)
 
   const inputRef = useRef()
 
@@ -45,37 +47,53 @@ const Search = () => {
   }
 
   useEffect(() => {
-    if (!debouncedSearchValue.trim()) {
-      setSearchResult([])
-      return
-    }
+    // if (!debouncedSearchValue.trim()) {
+    //   setSearchResult([])
+    //   return
+    // }
+
+    // const fetchApi = async () => {
+    //   setLoading(true)
+
+    //   const result = await searchService.search(debouncedSearchValue)
+    //   setSearchResult(result)
+
+    //   setLoading(false)
+    // }
 
     const fetchApi = async () => {
       setLoading(true)
 
-      const result = await searchService.search(debouncedSearchValue)
-      setSearchResult(result)
+      const querySnapshot = await getDocs(collection(db, 'users'))
+      setSearchResult(querySnapshot.docs)
 
       setLoading(false)
     }
 
     fetchApi()
-  }, [debouncedSearchValue])
+    // }, [debouncedSearchValue])
+  }, [])
 
   return (
     // Interactive tippy element may not be accessible via keyboard navigation
     // Using a wrapper <div> or <span> tag around the reference element solve this problem
     <div>
       <HeadlessTippy
-        visible={showResult && searchResult.length > 0}
+        visible={showResult && searchResult?.length > 0}
         interactive={true}
         render={(attrs) => (
           <div className={cx('search-result')} tabIndex="-1" {...attrs}>
             <PopperWrapper>
               <h4 className={cx('search-title')}>Accounts</h4>
-              {searchResult.map((result) => (
-                <AccountItem key={result.id} data={result} />
-              ))}
+              {searchResult.map((result) => {
+                /* return <AccountItem key={result.id} data={result} /> */
+
+                if (result.data().nickname.includes(searchValue)) {
+                  return <AccountItem key={result.id} data={result.data()} />
+                } else {
+                  return null
+                }
+              })}
             </PopperWrapper>
           </div>
         )}
